6639b4c1e74fb6523e47c0f6362dd65b5ce25675
baolong <1192384722@qq.com>
2022-11-06 14:49:04 +0800

    add 回文链表

--- golang_code/main.go
+++ golang_code/main.go
@@ -5,5 +5,6 @@ import (
 )
 
 func main() {
-	new_user_list.RunRomanToInt()
+	//new_user_list.RunRomanToInt()
+	new_user_list.RunIsPalindrome()
 }
---
+++ golang_code/new_user_list/palindrome_linked_list.go
@@ -0,0 +1,100 @@
+package new_user_list
+
+import (
+	"fmt"
+	"goland_code/utils"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func isPalindrome(head *ListNode) bool {
+	length := getLen(head)
+	if length == 0 {
+		return false
+	}
+
+	tmpHead := head
+	var reversedHead *ListNode
+	midIndex := length / 2
+
+	for i := 0; i < midIndex; i++ {
+		reversedHead, tmpHead = reverseIn(reversedHead, tmpHead)
+	}
+
+	if length%2 != 0 {
+		tmpHead = tmpHead.Next
+	}
+
+	return isSameLink(tmpHead, reversedHead)
+}
+
+func isSameLink(link1 *ListNode, link2 *ListNode) bool {
+	for link1 != nil && link2 != nil {
+		if link1.Val != link2.Val {
+			return false
+		}
+		link1 = link1.Next
+		link2 = link2.Next
+	}
+	if link1 == nil && link2 == nil {
+		return true
+	}
+
+	return false
+}
+
+func reverseIn(reHead *ListNode, head *ListNode) (*ListNode, *ListNode) {
+	if reHead == nil {
+		reHead = head
+		head = head.Next
+		reHead.Next = nil
+	} else {
+		tmpNode := head
+		head = head.Next
+		tmpNode.Next = reHead
+		reHead = tmpNode
+	}
+
+	return reHead, head
+}
+
+func getLen(head *ListNode) (length int) {
+	tmpHead := head
+	for tmpHead != nil {
+		length++
+		tmpHead = tmpHead.Next
+	}
+	return length
+}
+
+func RunIsPalindrome() {
+
+	var head ListNode
+	head.Val = 9
+	head.Next = &ListNode{
+		Val: 5,
+		Next: &ListNode{
+			Val: 5,
+			Next: &ListNode{
+				Val:  9,
+				Next: nil,
+			},
+		},
+	}
+
+	fmt.Printf("args |\t %v \n", head)
+	results := isPalindrome(&head)
+	fmt.Printf("resp |\t %v \n", results)
+	utils.PrintLine()
+}
